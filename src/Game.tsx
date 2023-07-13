4e09fca61647e8302c1460b43e823d478f6723b4
amir_mac <far20022002@mail.ru>
2023-07-13 16:09:28 +0500

    added jsx and props types

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 
-function Game() {
+
+type gameProps = {
+    question: string,
+    options: string[],
+    correct: number
+}
+function Game(props: gameProps) {
     return (
-        <div></div>
+        <div>
+            <h3>{props.question}</h3>
+            {props.options.map((op: string) => {
+                return (<div>
+                            {op}
+                        </div>)
+            })}
+        </div>
     );
 }
 
