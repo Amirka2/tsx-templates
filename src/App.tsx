426a128fedaa4e73f7c88e176337ffaf23ec7bbc
amir_mac <far20022002@mail.ru>
2023-07-13 16:31:45 +0500

    added steps changing in game

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
-import Game from "./Game";
+import {Game , gameProps} from "./Game";
 import Result from "./Result";
 
+
+const gameSteps: gameProps[] = [
+    {   question: 'aaa',
+        options: ['a', 'aa'],
+        correct: 0  },
+    {   question: 'bbb',
+        options: ['b', 'b'],
+        correct: 0  },
+]
+
 function App() {
+    const [step, setStep] = useState(0);
   return (
     <div className="App">
-      <Game question={'xxx'} options={['xxx', 'xx']} correct={0}></Game>
-      <Result result={1/10}></Result>
+        <Game question={gameSteps[step].question} options={gameSteps[step].options} correct={gameSteps[step].correct}></Game>
+      {/*<Result result={1/10}></Result>*/}
+        <button onClick={() => setStep(step - 1)}>Prev</button>
+        <button onClick={() => setStep(step + 1)}>Next</button>
     </div>
   );
 }
