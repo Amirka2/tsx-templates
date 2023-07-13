8c3e3524cc08a0b783503772021a68928fb22677
amir_mac <far20022002@mail.ru>
2023-07-13 16:31:14 +0500

    refactored game component

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,22 +1,19 @@
 import React from 'react';
 
 
-type gameProps = {
+export type gameProps = {
     question: string,
     options: string[],
     correct: number
 }
-function Game(props: gameProps) {
+export function Game(props: gameProps) {
+    const options = props.options.map((op: string) =>
+            <div>{op}</div>
+    );
     return (
         <div>
             <h3>{props.question}</h3>
-            {props.options.map((op: string) => {
-                return (<div>
-                            {op}
-                        </div>)
-            })}
+            {options}
         </div>
     );
 }
-
-export default Game;
